9e6e69802b58ee264aa9349021c9a2035f963562
Adeoluwa Adeboye <56544415+JUGG097@users.noreply.github.com>
2020-07-31 22:20:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from ncdcapi import ncdc
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(ncdc,"/")
